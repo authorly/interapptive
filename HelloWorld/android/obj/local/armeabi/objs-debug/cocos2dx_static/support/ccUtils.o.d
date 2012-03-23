fa31277e2cd88a680ce0ae2b35edf98a7b4dda52
minggo <ming@cocos2d-x.org>
2012-03-23 21:15:12 +0800

    delete unneeded files and can include font like <font/xxx>

--- HelloWorld/android/obj/local/armeabi/objs-debug/cocos2dx_static/support/ccUtils.o.d
+++
@@ -1,5 +0,0 @@
-/Users/chriswhitman/cocos2d-1.0.1-x-0.12.0/HelloWorld/android/obj/local/armeabi/objs-debug/cocos2dx_static/support/ccUtils.o: \
- /Users/chriswhitman/cocos2d-1.0.1-x-0.12.0/HelloWorld/android/jni/../../../cocos2dx/support/ccUtils.cpp \
- /Users/chriswhitman/cocos2d-1.0.1-x-0.12.0/HelloWorld/android/jni/../../../cocos2dx/support/ccUtils.h
-
-/Users/chriswhitman/cocos2d-1.0.1-x-0.12.0/HelloWorld/android/jni/../../../cocos2dx/support/ccUtils.h:
