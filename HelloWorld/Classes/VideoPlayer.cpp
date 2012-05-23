47aac55483f82fc5d8b3ece7b412d100c044efdc
minggo <ming@cocos2d-x.org>
2012-05-23 23:14:48 +0800

    play video ok on android

---
+++ HelloWorld/Classes/VideoPlayer.cpp
@@ -0,0 +1,46 @@
+#include "VideoPlayer.h"
+#include "platform/android/jni/JniHelper.h"
+#include "cocos2d.h"
+#include "jni.h"
+
+using namespace cocos2d;
+
+static VideoPlayer* g_videoPlayer = 0;
+
+VideoPlayer* VideoPlayer::sharedVideoPlayer()
+{
+    if (g_videoPlayer == 0)
+    {
+        g_videoPlayer = new VideoPlayer();
+    }
+    
+    return g_videoPlayer;
+}
+
+VideoPlayer::VideoPlayer() {}
+
+void VideoPlayer::playVideoByFilename(VideoPlayerProtocol *delegate, const char *fileName, bool showControl)
+{
+    JniMethodInfo methodInfo;
+    
+    if (JniHelper::getStaticMethodInfo(methodInfo, 
+                                       "org/cocos2dx/lib/Cocos2dxActivity",
+                                       "playVideoJNI",
+                                       "(Ljava/lang/String;Z)V")
+        )
+    {
+        // file name
+        jstring jstrFileName = methodInfo.env->NewStringUTF(fileName);
+        
+        // invoke java method
+        methodInfo.env->CallStaticVoidMethod(methodInfo.classID, methodInfo.methodID, jstrFileName, showControl);
+        
+        // release reference
+        methodInfo.env->DeleteLocalRef(jstrFileName);
+    }
+}
+
+void VideoPlayer::stopPlay()
+{
+
+}
