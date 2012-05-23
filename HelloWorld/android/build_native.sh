6211167cb97e59fcb1a0ad4adebe9e5439fe7298
Chris Whitman <chris@theoryplus.com>
2012-05-23 08:49:05 -0700

    Add local android-ndk and source path to build_native.sh

--- HelloWorld/android/build_native.sh
+++ HelloWorld/android/build_native.sh
@@ -1,6 +1,8 @@
 # set params
 NDK_ROOT_LOCAL=/cygdrive/d/programe/android/ndk/android-ndk-r6b
 COCOS2DX_ROOT_LOCAL=/Users/zhangkoumyou/SourceCode/interapptive
+# NDK_ROOT_LOCAL=/Users/chriswhitman/workspace/android-ndk-r6b
+# COCOS2DX_ROOT_LOCAL=/Users/chriswhitman/workspace/interapptive
 
 # try to get global variable
 if [ $NDK_ROOT"aaa" != "aaa" ]; then
