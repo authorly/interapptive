a7ab76ace050a3dcd1756622da97731027797367
minggo <ming@cocos2d-x.org>
2012-08-01 20:46:04 +0800

    make background audio can be removed from main menu

--- HelloWorld/Classes/JsonParser.cpp
+++ HelloWorld/Classes/JsonParser.cpp
@@ -69,7 +69,10 @@ void JsonParser::parseMainMenu(Json::Value &root)
     
     // parse audio
     Json::Value audio = mainMenu["audio"];
-    parseMainMenuAodio(audio);
+    if (! audio.isNull())
+    {
+        parseMainMenuAodio(audio);
+    }
     
     // parse CCSprites
     Json::Value sprites = mainMenu["CCSprites"];
@@ -91,13 +94,22 @@ void JsonParser::parseMainMenu(Json::Value &root)
 void JsonParser::parseMainMenuAodio(Json::Value &value)
 {
     // backgroundMusic
-    MainMenu::audio.backgroundMusic = value["backgroundMusic"].asCString();
-    // backgroundMusicLoops
-    MainMenu::audio.backgroundMusicLoops = value["backgroundMusicLoops"].asInt();
-    // soundEffect
-    MainMenu::audio.soundEffect = value["soundEffect"].asCString();
-    // soundEffectLoops
-    MainMenu::audio.soundEffectLoops = value["soundEffectLoops"].asInt();
+    Json::Value backgroundJson = value["backgroundMusic"];
+    if (! backgroundJson.isNull())
+    {
+        MainMenu::audio.backgroundMusic = backgroundJson.asCString();
+        // backgroundMusicLoops
+        MainMenu::audio.backgroundMusicLoops = value["backgroundMusicLoops"].asInt();
+    }
+    
+    Json::Value soundJson = value["soundEffect"];
+    if (! soundJson.isNull())
+    {
+        // soundEffect
+        MainMenu::audio.soundEffect = soundJson.asCString();
+        // soundEffectLoops
+        MainMenu::audio.soundEffectLoops = value["soundEffectLoops"].asInt();
+    }    
 }
 
 void JsonParser::parseMainMenuSprites(Json::Value &value)
