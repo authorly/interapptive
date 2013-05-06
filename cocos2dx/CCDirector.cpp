1733c669e9711119fd50444b37afd6a23958ffcb
Chris Whitman <christopherscottwhitman@gmail.com>
2013-05-05 21:27:57 -0700

    Use black for background

--- cocos2dx/CCDirector.cpp
+++ cocos2dx/CCDirector.cpp
@@ -207,8 +207,6 @@ void CCDirector::drawScene(void)
 	{
 		CCScheduler::sharedScheduler()->tick(m_fDeltaTime);
 	}
-    
-    glClearColor(1, 1, 1, 1);
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     
