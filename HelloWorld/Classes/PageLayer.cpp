ae08babcb3d5ebdad5aeb086214ff753d23f78d9
Waseem Ahmad <talk.to.waseem@gmail.com>
2013-07-10 08:58:20 +0530

    Fixes old merge conflict.

--- HelloWorld/Classes/PageLayer.cpp
+++ HelloWorld/Classes/PageLayer.cpp
@@ -925,11 +925,7 @@ void PageLayer::doSwipeLeftAfterDelay(CCObject *sender)
 void PageLayer::stopHighlightEffect()
 {
     // stop all effect, I think now it will only have one effect that speaking word.
-<<<<<<< HEAD
-    SimpleAudioEngine::sharedEngine()->stopBackgroundMusic(false, true);
-=======
     SimpleAudioEngine::sharedEngine()->stopAllBackgroundMusic();
->>>>>>> 64f79b563e3b83e131defd0d0c31923a7ad19fd4
     SimpleAudioEngine::sharedEngine()->stopAllEffects();
 }
 
