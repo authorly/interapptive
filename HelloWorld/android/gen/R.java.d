bc6e0f2e0730f7b24ce8186307b66889ba720e10
Chris Whitman <christopherscottwhitman@gmail.com>
2013-10-18 11:49:07 -0700

    Change app name and icons for SITW Android release

---
+++ HelloWorld/android/gen/R.java.d
@@ -0,0 +1,19 @@
+/Users/whitman/rails_apps/Crucible/HelloWorld/android/gen/org/cocos2dx/application/R.java \
+ : /Users/whitman/rails_apps/Crucible/HelloWorld/android/res/drawable-hdpi/icon.png \
+/Users/whitman/rails_apps/Crucible/HelloWorld/android/res/drawable-ldpi/icon.png \
+/Users/whitman/rails_apps/Crucible/HelloWorld/android/res/drawable-mdpi/icon.png \
+/Users/whitman/rails_apps/Crucible/HelloWorld/android/res/layout/helloworld_demo.xml \
+/Users/whitman/rails_apps/Crucible/HelloWorld/android/res/layout/videoplayer.xml \
+/Users/whitman/rails_apps/Crucible/HelloWorld/android/res/raw/video_chickadee_on_hat.mp4 \
+/Users/whitman/rails_apps/Crucible/HelloWorld/android/res/raw/video_doe_snort.mp4 \
+/Users/whitman/rails_apps/Crucible/HelloWorld/android/res/raw/video_flail.mp4 \
+/Users/whitman/rails_apps/Crucible/HelloWorld/android/res/raw/video_mixed_nuts.mp4 \
+/Users/whitman/rails_apps/Crucible/HelloWorld/android/res/raw/video_muskrat.mp4 \
+/Users/whitman/rails_apps/Crucible/HelloWorld/android/res/raw/video_owl.mp4 \
+/Users/whitman/rails_apps/Crucible/HelloWorld/android/res/raw/video_the_end.mp4 \
+/Users/whitman/rails_apps/Crucible/HelloWorld/android/res/raw/video_the_snowman.mp4 \
+/Users/whitman/rails_apps/Crucible/HelloWorld/android/res/values/strings.xml \
+/Users/whitman/rails_apps/Crucible/HelloWorld/android/bin/res/drawable-hdpi/icon.png \
+/Users/whitman/rails_apps/Crucible/HelloWorld/android/bin/res/drawable-ldpi/icon.png \
+/Users/whitman/rails_apps/Crucible/HelloWorld/android/bin/res/drawable-mdpi/icon.png \
+/Users/whitman/rails_apps/Crucible/HelloWorld/android/bin/AndroidManifest.xml \
