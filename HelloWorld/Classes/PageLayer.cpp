d910b236382707b21ff6b9333b98ef342e7e8be6
minggo <ming@cocos2d-x.org>
2014-02-19 22:38:30 +0800

    (#340)prealod effect before playing

--- HelloWorld/Classes/PageLayer.cpp
+++ HelloWorld/Classes/PageLayer.cpp
@@ -251,6 +251,7 @@ void PageLayer::doHotspotTouched(HotspotInfo *hotspot,bool isParagraphHotspot)
         // stop first
         SimpleAudioEngine::sharedEngine()->stopEffect(touchSoundId);
         // play
+        SimpleAudioEngine::sharedEngine()->preloadEffect(audeoName.c_str());
         touchSoundId = SimpleAudioEngine::sharedEngine()->playEffect(audeoName.c_str());
     }
 }
