98396e191b2756526a59c05a8217395258989b3d
Chris Whitman <christopherscottwhitman@gmail.com>
2014-03-07 13:40:29 -0800

    Animal Motions app fix

--- HelloWorld/android/gen/R.java.d
+++ HelloWorld/android/gen/R.java.d
@@ -5,6 +5,9 @@
 /Users/whitman/rails_apps/Crucible/HelloWorld/android/res/drawable-xhdpi/icon.png \
 /Users/whitman/rails_apps/Crucible/HelloWorld/android/res/layout/helloworld_demo.xml \
 /Users/whitman/rails_apps/Crucible/HelloWorld/android/res/layout/videoplayer.xml \
-/Users/whitman/rails_apps/Crucible/HelloWorld/android/res/raw/sample.mp4 \
 /Users/whitman/rails_apps/Crucible/HelloWorld/android/res/values/strings.xml \
+/Users/whitman/rails_apps/Crucible/HelloWorld/android/bin/res/drawable-hdpi/icon.png \
+/Users/whitman/rails_apps/Crucible/HelloWorld/android/bin/res/drawable-ldpi/icon.png \
+/Users/whitman/rails_apps/Crucible/HelloWorld/android/bin/res/drawable-mdpi/icon.png \
+/Users/whitman/rails_apps/Crucible/HelloWorld/android/bin/res/drawable-xhdpi/icon.png \
 /Users/whitman/rails_apps/Crucible/HelloWorld/android/bin/AndroidManifest.xml \
