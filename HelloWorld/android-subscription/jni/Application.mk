a979db652badc1d56c40ddca6f09c80a571d6e5d
minggo <ming@cocos2d-x.org>
2014-03-09 14:02:33 +0800

    remove android-subscription

--- HelloWorld/android-subscription/jni/Application.mk
+++
@@ -1,6 +0,0 @@
-APP_STL := gnustl_static
-APP_CPPFLAGS += -frtti
-# json use exception
-APP_CPPFLAGS += -fexceptions
-
-APP_MODULES := cocos2dx_static chipmunk_shared cocosdenshion_shared game_logic_static helloworld_shared
