a476fc92b5b8c25d92f3b07b04356a0b4f3059e1
minggo <ming@cocos2d-x.org>
2014-03-12 00:04:28 +0800

    don't consider retina when calculation dpi

--- HelloWorld/Classes/Utils.mm
+++ HelloWorld/Classes/Utils.mm
@@ -12,17 +12,31 @@
 
 int Utils::getDPI()
 {
-    float scale = 1;
-    if ([[UIScreen mainScreen] respondsToSelector:@selector(scale)]) {
-        scale = [[UIScreen mainScreen] scale];
-    }
-    float dpi;
+//    float scale = 1;
+//    if ([[UIScreen mainScreen] respondsToSelector:@selector(scale)]) {
+//        scale = [[UIScreen mainScreen] scale];
+//    }
+//    float dpi;
+//    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+//        dpi = 132 * scale;
+//    } else if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
+//        dpi = 163 * scale;
+//    } else {
+//        dpi = 160 * scale;
+//    }
+//    
+//    return dpi;
+    
+    // Don't have to consider retina, becuase touch Point value is the same
+    // no matter it is retina or not.
+    
+    float dpi = 0.f; // dpi of iPad normal
     if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
-        dpi = 132 * scale;
+        dpi = 132;
     } else if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
-        dpi = 163 * scale;
+        dpi = 163;
     } else {
-        dpi = 160 * scale;
+        dpi = 160;
     }
     
     return dpi;
