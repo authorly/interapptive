84f5dfe1e1dc27525016e388094e8a571c374109
Chris Whitman <christopherscottwhitman@gmail.com>
2014-03-19 11:51:20 -0700

    (#351) Repeatable background - TODO:  shelves to be placed on wood background for repeating

--- HelloWorld/Classes/LoadingLayer.cpp
+++ HelloWorld/Classes/LoadingLayer.cpp
@@ -13,6 +13,14 @@ using namespace cocos2d;
 
 LoadingLayer::LoadingLayer()
 {
+    CCTexture2D *texture = CCTextureCache::sharedTextureCache()->addImage("background-wood.png");
+    ccTexParams texParams = { GL_LINEAR,GL_LINEAR,GL_REPEAT,GL_REPEAT };
+	texture->setTexParameters(&texParams);
+    CCSprite *bgImage = CCSprite::spriteWithTexture(texture, CCRectMake(0, 0, 1024, 768));
+    bgImage->setPosition(ccp(0, 0));
+    bgImage->setAnchorPoint(ccp(0, 0));
+    addChild(bgImage);
+    
     CCMenuItemFont *book1 = CCMenuItemFont::itemFromString("book1", this, menu_selector(LoadingLayer::buttonClicked));
     book1->setPosition(ccp(100, 100));
     CCMenu *menu = CCMenu::menuWithItem(book1);
