6310d1d29dc5d044dcf158e7d999ec915753f5e4
Chris Whitman <christopherscottwhitman@gmail.com>
2014-04-07 11:29:46 -0700

    Support more Android devices (including Kindle 1st gen)

---
+++ HelloWorld/android/gen/R.java.d
@@ -0,0 +1,9 @@
+/Users/whitman/rails_apps/Crucible/HelloWorld/android/gen/org/authorly/animalmotions/R.java \
+ : /Users/whitman/rails_apps/Crucible/HelloWorld/android/res/drawable-hdpi/icon.png \
+/Users/whitman/rails_apps/Crucible/HelloWorld/android/res/drawable-ldpi/icon.png \
+/Users/whitman/rails_apps/Crucible/HelloWorld/android/res/drawable-mdpi/icon.png \
+/Users/whitman/rails_apps/Crucible/HelloWorld/android/res/drawable-xhdpi/icon.png \
+/Users/whitman/rails_apps/Crucible/HelloWorld/android/res/layout/helloworld_demo.xml \
+/Users/whitman/rails_apps/Crucible/HelloWorld/android/res/layout/videoplayer.xml \
+/Users/whitman/rails_apps/Crucible/HelloWorld/android/res/values/strings.xml \
+/Users/whitman/rails_apps/Crucible/HelloWorld/android/bin/AndroidManifest.xml \
