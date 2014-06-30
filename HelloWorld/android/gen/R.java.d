8fa7604a078807c686026c4dd58b9581421cd02b
Authorly Deployer <info@authorly.com>
2014-06-30 18:33:41 +0000

    (#393) Crashing branch.

---
+++ HelloWorld/android/gen/R.java.d
@@ -0,0 +1,14 @@
+/home/rails/apps/authorly/Crucible/HelloWorld/android/gen/org/cocos2dx/application/R.java \
+ : /home/rails/apps/authorly/Crucible/HelloWorld/android/res/drawable-hdpi/icon.png \
+/home/rails/apps/authorly/Crucible/HelloWorld/android/res/drawable-ldpi/icon.png \
+/home/rails/apps/authorly/Crucible/HelloWorld/android/res/drawable-xhdpi/icon.png \
+/home/rails/apps/authorly/Crucible/HelloWorld/android/res/values/strings.xml \
+/home/rails/apps/authorly/Crucible/HelloWorld/android/res/layout/videoplayer.xml \
+/home/rails/apps/authorly/Crucible/HelloWorld/android/res/layout/helloworld_demo.xml \
+/home/rails/apps/authorly/Crucible/HelloWorld/android/res/drawable-mdpi/icon.png \
+/home/rails/apps/authorly/Crucible/HelloWorld/android/res/raw/sample.mp4 \
+/home/rails/apps/authorly/Crucible/HelloWorld/android/bin/res/drawable-hdpi/icon.png \
+/home/rails/apps/authorly/Crucible/HelloWorld/android/bin/res/drawable-ldpi/icon.png \
+/home/rails/apps/authorly/Crucible/HelloWorld/android/bin/res/drawable-xhdpi/icon.png \
+/home/rails/apps/authorly/Crucible/HelloWorld/android/bin/res/drawable-mdpi/icon.png \
+/home/rails/apps/authorly/Crucible/HelloWorld/android/bin/AndroidManifest.xml \
